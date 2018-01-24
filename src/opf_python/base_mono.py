617610d73d2766f75c4341a2ffca358991f2c252
Johnnyc1423 <JohnEdChristensen@gmail.com>
2018-01-24 13:20:03 -0700

    Updated /notes and updated base_mono_37_39 with a faster algorithim

--- src/opf_python/base_mono.py
+++ src/opf_python/base_mono.py
@@ -275,11 +275,11 @@ def base_mono_41(n):
     return spHNFs
                           
 def base_mono_37_39(n):
-    """Finds the symmetry preserving HNFs for the base centered monoclinic
+  """Finds the symmetry preserving HNFs for the base centered monoclinic
     lattices with a determinant of n. Assuming for basis 37 A =
-    [[-1.79092 , -1.47209 , 0.790922], [ 1.  , 0.  , 1.  ], [ 1.  ,
-    -1.41421 , -1.  ]], for basis 39 A = [[ 0.  , 1.73205 , 1.  ],
-    [-1.  , 0.  , -1.  ], [ 1.66542 , 0.672857, -1.66542 ]].
+    [[-1.79092,-1.47209,0.790922],[1.0,-1.41421,-1.0],[1.0,0.0,1.0]], for basis
+    39 A = [[0, -1.73205,-1],[-1.66542, -0.672857, 1.66542], [1,0,1]], for 
+    basis 41 A = [[-1.85397, -0.854143, 1.35397],[1, 0, 1],[[1, -1.41421, -1]].
 
     Args:
         n (int): The determinant of the HNFs.
@@ -299,22 +299,21 @@ def base_mono_37_39(n):
         a = diag[0]
         c = diag[1]
         f = diag[2]
-
+        #gamma 2
         if f%2==0:
             es = [0,f//2.]
         else:
             es = [0]
 
         for e in es:
-            for b in range(c):
-                b11 = -a+2*b
-                g11 = -b11*e/float(c)
-                if b11%c==0 and g11%f==0:
-                    for d in range(f):
+            for d in range(f):
+                g11 = -a+2*d
+                if g11%f==0:
+                    for b in range(c):
                         HNF = [[a,0,0],[b,c,0],[d,e,f]]
                         spHNFs.append(HNF)
                                 
-    return spHNFs
+    return spHNFs   
                           
 def base_mono_43(n):
     """Finds the symmetry preserving HNFs for the base centered monoclinic
